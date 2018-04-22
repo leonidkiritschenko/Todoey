3bc6347ececd7a2836c6bd154c15807748b0fd70
sinner89 <l.kiritschenko@gmail.com>
2018-04-22 15:58:14 +0200

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Leonid Kiritschenko on 2018-04-16.
+//  Copyright © 2018 Leonid Kiritschenko. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
